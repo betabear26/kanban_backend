293123ef85d961fe0dc4b7f4c16b81b723cb1570
Sandeep Suman <betabear26@gmail.com>
2019-12-06 02:40:37 +0530

    Settings file created

---
+++ kanban/src/kanban/settings/dev.py
@@ -0,0 +1,10 @@
+from .base import *
+
+# SECURITY WARNING: keep the secret key used in production secret!
+# This is a dummy secret key and it will not work
+SECRET_KEY = ''
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
---
+++ kanban/src/kanban/settings/production.py
@@ -0,0 +1,11 @@
+from .base import *
+
+# SECURITY WARNING: keep the secret key used in production secret!
+# This is a dummy secret key and it will not work
+SECRET_KEY = ''
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+
+ALLOWED_HOSTS = []
