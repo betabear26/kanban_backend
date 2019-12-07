ad6b83fe9ed3775e935d28bfe93e614a780d2f03
Sandeep Suman <betabear26@gmail.com>
2019-12-07 12:26:45 +0530

    Removed keys and generated new ones

--- kanban/src/kanban/settings/production.py
+++ kanban/src/kanban/settings/production.py
@@ -2,7 +2,7 @@ from .base import *
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # This is a dummy secret key and it will not work
-SECRET_KEY = 'qck)@xx$&d$8y2r@37(m9_)rjf2h67$#0no$$7q1g!#x84o@if'
+SECRET_KEY = os.environ['KANBAN_PROD_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
