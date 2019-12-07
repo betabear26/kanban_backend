09a5461789e218d5136594d614eb9d57e40a319d
Sandeep Suman <betabear26@gmail.com>
2019-12-07 11:29:17 +0530

    test

--- kanban/src/kanban/settings/production.py
+++ kanban/src/kanban/settings/production.py
@@ -2,7 +2,7 @@ from .base import *
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # This is a dummy secret key and it will not work
-SECRET_KEY = ''
+SECRET_KEY = 'qck)@xx$&d$8y2r@37(m9_)rjf2h67$#0no$$7q1g!#x84o@if'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
